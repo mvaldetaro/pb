bcb970dbc1d043c1bd5eb62cbd127d53997451e1
Magno Valdetaro <mvaldetaro@gmail.com>
2018-09-05 21:47:32 -0300

    Add componnente "Podcasts"

---
+++ gsc-app/src/componentes/Podcasts/Podcast.js
@@ -0,0 +1,34 @@
+import React, {Component, Fragment} from 'react';
+
+class Podcast extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      //podcast: {}
+    };
+
+  };
+
+  componentDidMount() {
+    this.setState(this.props.podcast)
+  }
+
+  render() {
+
+    const {thumbnail_podcasts, titulo_podcasts, url_podcasts} = this.state;
+
+    return (
+      <Fragment>
+        <a href={url_podcasts}>
+          <img src={thumbnail_podcasts} alt={titulo_podcasts} width='200px'/>
+        </a>
+        <h3>{titulo_podcasts}</h3>
+
+      </Fragment>
+    )
+  }
+}
+
+export default Podcast;
---
+++ gsc-app/src/componentes/Podcasts/Podcasts.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import Podcast from "./Podcast";
+
+class Podcasts extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      podcasts: []
+    };
+
+  };
+
+  componentDidUpdate(prevProps) {
+    if (this.props.data !== prevProps.data) {
+      this.setState({podcasts: this.props.data})
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Podcasts</h2>
+        {this.state.podcasts
+          ? (
+            <ul>
+              {this
+                .state
+                .podcasts
+                .map((currentPodcast, i) => (
+                  <li key={i}>
+                    <Podcast podcast={currentPodcast}/>
+                  </li>
+                ))}
+            </ul>
+          )
+          : "Nenhum podcast encontrado"}
+      </div>
+    )
+  }
+}
+
+export default Podcasts;
