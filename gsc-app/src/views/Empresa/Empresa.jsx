cba3e9c0576472127338e82343b7c038d2a0f015
Magno Valdetaro <mvaldetaro@gmail.com>
2018-09-30 15:54:19 -0300

    Add componente empresa

---
+++ gsc-app/src/views/Empresa/Empresa.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Empresa extends Component {
+  render() {
+    return (
+      <div className="app">
+        <p>Empresa</p>
+      </div>
+    );
+  }
+}
+
+export default Empresa;
