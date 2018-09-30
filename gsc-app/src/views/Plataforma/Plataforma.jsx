bf8ddc02ae50380ba0836cb35e9a1ef8faad0f91
Magno Valdetaro <mvaldetaro@gmail.com>
2018-09-30 15:54:01 -0300

    Add o componente Plataforma

---
+++ gsc-app/src/views/Plataforma/Plataforma.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Plataforma extends Component {
+  render() {
+    return (
+      <div className="app">
+        <p>Plataforma</p>
+      </div>
+    );
+  }
+}
+
+export default Plataforma;
