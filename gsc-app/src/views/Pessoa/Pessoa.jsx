8827b52ea0e5603926c9dcd3b85ee2bca75d83af
Magno Valdetaro <mvaldetaro@gmail.com>
2018-09-30 15:53:44 -0300

    Add compoente Pessoa

---
+++ gsc-app/src/views/Pessoa/Pessoa.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Pessoa extends Component {
+  render() {
+    return (
+      <div className="app">
+        <p>Pessoa</p>
+      </div>
+    );
+  }
+}
+
+export default Pessoa;
